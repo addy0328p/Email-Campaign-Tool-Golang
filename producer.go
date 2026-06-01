a67f6b74da552ed81624b0061b28a15e84b6345f
Aditya Pandey <aditya12bone@gmail.com>
2026-06-01 17:42:47 +0530

    Refactor loadRecipient to use channel for recipients
    Modified loadRecipient function to accept a channel for sending Recipient structs.

--- producer.go
+++ producer.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"os"
 )
 
-func loadRecipient(filePath string) error {
+func loadRecipient(filePath string, ch chan Recipient) error {
+	defer close(ch)
+
 	f, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	reader := csv.NewReader(f)
-	records, err := reader.ReadAll()
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
 	if err != nil {
 		return err
 	}
 
 	for _, record := range records[1:] {
-		fmt.Println(record)
+		ch <- Recipient{
+			Name:  record[0],
+			Email: record[1],
+		}
 	}
+
 	return nil
 }
