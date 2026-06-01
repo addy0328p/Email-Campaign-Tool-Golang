382fb5fde2bd2e5dfe7f42fc8a6b6c79119b8c30
Aditya Pandey <aditya12bone@gmail.com>
2026-06-01 17:43:07 +0530

    Implement concurrent email sending with goroutines
    Refactor main function to use goroutines and wait groups for concurrent email processing.

--- main.go
+++ main.go
@@ -1,10 +1,59 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"sync"
+)
+
 type Recipient struct {
 	Name  string
 	Email string
 }
 
 func main() {
-	loadRecipient("./emails.csv")
+	recipientChannel := make(chan Recipient)
+
+	go func() {
+		loadRecipient("./emails.csv", recipientChannel)
+	}()
+
+	var wg sync.WaitGroup
+	workerCount := 5
+
+	for i := 1; i <= workerCount; i++ {
+		wg.Add(1)
+		go emailWorker(i, recipientChannel, &wg)
+	}
+
+	wg.Wait()
+}
+
+func executeTemplate(r Recipient) (string, error) {
+	t, err := template.ParseFiles("email.tmpl")
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+
+	err = t.Execute(&tpl, r)
+	if err != nil {
+		return "", err
+	}
+
+	msg := fmt.Sprintf(
+		"From: Aditya Kumar <ourangseb2@gmail.com>\r\n"+
+			"To: %s\r\n"+
+			"Subject: Greetings %s\r\n"+
+			"MIME-Version: 1.0\r\n"+
+			"Content-Type: text/html; charset=UTF-8\r\n\r\n"+
+			"%s",
+		r.Email,
+		r.Name,
+		tpl.String(),
+	)
+
+	return msg, nil
 }
